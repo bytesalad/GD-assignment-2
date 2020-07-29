0a6e740d92a37069c9cfd14b307d3e178f11fd07
bytesalad <52949496+bytesalad@users.noreply.github.com>
2020-07-29 17:54:20 +0200

    Integrate assets and follow player camera

---
+++ Assets/Scripts/PlayerCameraFollow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private float followSpeed;
+
+    private void Update()
+    {
+        gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, 
+            new Vector3(player.position.x, player.position.y, gameObject.transform.position.z), followSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/PlayerFlashlight.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class PlayerFlashlight : MonoBehaviour
+{
+    [SerializeField] private TilemapRenderer trapRenderer;
+    [SerializeField] private float timeActive;
+    [SerializeField] private float timeRecharge;
+    [SerializeField] private KeyCode flashlightKey;
+    private float time = 0;
+    private bool active = false;
+
+    private void Start()
+    {
+        time = timeRecharge;
+    }
+
+    private void Update()
+    {
+        if (!active)
+        {
+            if (time < timeRecharge)
+            {
+                time += Time.deltaTime;
+            }
+            else
+            {
+                if (Input.GetKeyDown(flashlightKey))
+                {
+                    active = true;
+                    time = 0;
+                    //activate renderers.
+                    Debug.Log("Flashlight activated!");
+                }
+            }
+        }
+        else
+        {
+            if (time < timeActive)
+            {
+                time += Time.deltaTime;
+            }
+            else
+            {
+                active = false;
+                //deactivate renderers again.
+            }
+        }
+    }
+}
