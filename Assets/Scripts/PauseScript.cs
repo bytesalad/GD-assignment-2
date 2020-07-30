f57ebbad3992db16a38f68716617688a0c2adc5d
Brendan Jackson <brendanjj50@gmail.com>
2020-07-30 16:08:29 +0200

    Added Pause Menu

---
+++ Assets/Scripts/PauseScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseScript : MonoBehaviour
+{
+    public GameObject PauseMenu;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseGame();
+            
+        }
+    }
+
+    void PauseGame()
+    {
+        Time.timeScale = 0;
+        PauseMenu.SetActive(true);
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+        PauseMenu.SetActive(false);
+    }
+
+    public void QuitLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
